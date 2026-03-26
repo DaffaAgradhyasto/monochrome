0f4725c0d3ce3ce6087179a472c5ac61490846ae
Xarcers Hoshi <xarcers@proton.me>
2026-03-26 08:49:30 +0700

    fix: remove extra closing brace at line 270

--- js/mood-playlist-ui.js
+++ js/mood-playlist-ui.js
@@ -267,6 +267,5 @@ export class MoodPlaylistUI {
 					return `#${Math.round(r).toString(16).padStart(2, '0')}${Math.round(g).toString(16).padStart(2, '0')}${Math.round(b).toString(16).padStart(2, '0')}`;
 				}
 	}
-}
 
 	
